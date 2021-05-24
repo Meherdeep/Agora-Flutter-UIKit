b7b4a6df50a7ddb9c7e26e694a670b307bd5e169
Tadas Petra <tadas@tadaspetra.com>
2021-05-24 14:49:07 -0500

    fixing main.dart

---
+++ example/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:agora_flutter_uikit/agora_flutter_uikit.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AgoraClient client = AgoraClient(
+    appId: "<----Insert App ID ---->",
+    enabledPermission: [
+      Permission.camera,
+      Permission.microphone,
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Agora Flutter UIKit'),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              AgoraVideoViewer(
+                client: client,
+              ),
+              AgoraVideoButtons(
+                client: client,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
